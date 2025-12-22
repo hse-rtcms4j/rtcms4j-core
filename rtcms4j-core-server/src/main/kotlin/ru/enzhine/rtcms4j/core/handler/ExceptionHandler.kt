53fe9d4ea909bc170563a3c07d01c87b532aeee1
Enzhine <onarushkashamaev@gmail.com>
2025-12-23 00:20:19 +0300

    Verbose validation errors

--- rtcms4j-core-server/src/main/kotlin/ru/enzhine/rtcms4j/core/handler/ExceptionHandler.kt
+++ rtcms4j-core-server/src/main/kotlin/ru/enzhine/rtcms4j/core/handler/ExceptionHandler.kt
@@ -1,11 +1,15 @@
 package ru.enzhine.rtcms4j.core.handler
 
+import jakarta.validation.ConstraintViolationException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.HandlerMethodValidationException
 import ru.enzhine.rtcms4j.core.api.dto.ErrorResponseDto
+import ru.enzhine.rtcms4j.core.builder.humanizeError
 import ru.enzhine.rtcms4j.core.builder.newErrorResponseEntity
 import ru.enzhine.rtcms4j.core.controller.CoreController
 import ru.enzhine.rtcms4j.core.exception.ConditionFailureException
@@ -16,31 +20,54 @@ class ExceptionHandler {
 
     @ExceptionHandler(value = [ConditionFailureException::class])
     fun conditionFailureExceptionHandler(ex: ConditionFailureException): ResponseEntity<ErrorResponseDto> {
-        logger.warn("ConditionFailureException reached.", ex)
         return when (ex) {
             is ConditionFailureException.NotFound ->
                 newErrorResponseEntity(
-                    HttpStatus.NOT_FOUND,
-                    ex.detailCode,
-                    ex.message,
+                    httpStatus = HttpStatus.NOT_FOUND,
+                    detailCode = ex.detailCode,
+                    detailMessage = ex.message,
                 )
 
             is ConditionFailureException.KeyDuplicated ->
                 newErrorResponseEntity(
-                    HttpStatus.CONFLICT,
-                    ex.detailCode,
-                    ex.message,
+                    httpStatus = HttpStatus.CONFLICT,
+                    detailCode = ex.detailCode,
+                    detailMessage = ex.message,
                 )
 
             is ConditionFailureException.ForbiddenAccess ->
                 newErrorResponseEntity(
-                    HttpStatus.FORBIDDEN,
-                    ex.detailCode,
-                    ex.message,
+                    httpStatus = HttpStatus.FORBIDDEN,
+                    detailCode = ex.detailCode,
+                    detailMessage = ex.message,
                 )
 
             else -> newErrorResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
         }
+    }
+
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun methodArgumentNotValidExceptionHandler(ex: MethodArgumentNotValidException): ResponseEntity<ErrorResponseDto> {
+        return newErrorResponseEntity(
+            httpStatus = HttpStatus.BAD_REQUEST,
+            detailMessage = humanizeError(ex),
+        )
+    }
+
+    @ExceptionHandler(value = [HandlerMethodValidationException::class])
+    fun handlerMethodValidationExceptionHandler(ex: HandlerMethodValidationException): ResponseEntity<ErrorResponseDto> {
+        return newErrorResponseEntity(
+            httpStatus = HttpStatus.BAD_REQUEST,
+            detailMessage = humanizeError(ex),
+        )
+    }
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun constraintViolationExceptionHandler(ex: ConstraintViolationException): ResponseEntity<ErrorResponseDto> {
+        return newErrorResponseEntity(
+            httpStatus = HttpStatus.BAD_REQUEST,
+            detailMessage = ex.localizedMessage,
+        )
     }
 
     @ExceptionHandler

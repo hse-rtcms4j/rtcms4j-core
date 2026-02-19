c7b1609961ad3fcfeaf4b4e3534451b4b6e1305c
Enzhine <onarushkashamaev@gmail.com>
2026-02-19 22:37:12 +0300

    Local container building

--- rtcms4j-core-server/src/main/kotlin/ru/enzhine/rtcms4j/core/config/SecurityConfig.kt
+++ rtcms4j-core-server/src/main/kotlin/ru/enzhine/rtcms4j/core/config/SecurityConfig.kt
@@ -2,10 +2,8 @@ package ru.enzhine.rtcms4j.core.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.web.cors.CorsUtils
 import ru.enzhine.rtcms4j.core.security.JwtKeycloakPrincipalConverter
 
 @Configuration
